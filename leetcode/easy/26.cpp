b4d899d5620a1e34a6e9aef2072d124d52c0a752
Bogdan Ivanyuk <ivanyuk.bogdan1999@yahoo.com>
2021-06-22 22:23:01 +0300

    added new & fixed style

---
+++ leetcode/easy/26.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int removeDuplicates(vector<int>& nums) {
+        if (!nums.size()){
+            return 0;
+        }
+        int i = 0, j = 0;
+        while (j < nums.size()){
+            if (nums[i] != nums[j]){
+                nums[++i] = nums[j];
+            }
+            ++j;
+        }
+        return i + 1;
+    }
+};
+
+
